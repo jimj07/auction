1e9bb0b3c0ff626328bfff9b8a0f837c0521f04d
jim <jim.jiang@domain.com.au>
2018-12-02 14:58:30 +1100

    added header styling

--- client/src/screens/ItemDetails.js
+++ client/src/screens/ItemDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AuctionItemApi from '../api/AuctionItemApi';
+import './ItemDetails.css';
 
 class ItemDetails extends Component {
   state = {};
@@ -19,18 +20,30 @@ class ItemDetails extends Component {
     }
 
     return (
-      <div className="auction-item-details">
-        <Link to="/">Back</Link>
-        <div className="auction-item-gallery" />
-        <div classNem="auction-item-right">
-          <h2>{item.name}</h2>
+      <div className="detail-wrapper">
+        <Link to="/" className="btn--back">
+          Back
+        </Link>
+        <div className="auction-item-details">
+          <div className="auction-item-gallery">
+            <img
+              className="auction-item-img"
+              src={item.imageSrc}
+              alt={`item ${item.name}`}
+            />
+          </div>
+          <div className="auction-item-right">
+            <h2>{item.name}</h2>
 
-          <div className="auction-item-bidding">
-            <div className="auction-item-current-price" />
-            <form className="auction-item-bidding-action">
-              <div className="auction-item-bidding-input" />
-              <button className="auction-item-bidding-button" />
-            </form>
+            <div className="auction-item-bidding">
+              <div className="auction-item-current-price">$299.9</div>
+              <form className="auction-item-bidding-action">
+                <div className="auction-item-bidding-input" />
+                <button className="auction-item-bidding-button btn--back">
+                  Bid
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
